4cac4391e5768a7f3918ebb8f9bd20efbbb3b6f8
H.Y.C <chenghsienyu@aim.com>
2019-08-25 21:19:40 +0800

    add basic function
    add basic function

---
+++ demo.py
@@ -0,0 +1,43 @@
+# Simple test for NeoPixels on Raspberry Pi
+import time
+import board
+import neopixel
+
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 30
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+                           pixel_order=ORDER)
+
+
+# def rainbow_cycle(wait):
+#     for j in range(255):
+#         for i in range(num_pixels):
+#             pixel_index = (i * 256 // num_pixels) + j
+#             pixels[i] = wheel(pixel_index & 255)
+#         pixels.show()
+#         time.sleep(wait)
+
+
+while True:
+
+    for i in range(255):
+        pixels.fill((i, i, i))
+        pixels.show()
+        if i == 255:
+            for i in reversed(range(255)):
+                pixels.fill((i, i, i))
+                pixels.show()
+
+time.sleep(1)
+
+#    rainbow_cycle(0.001)    # rainbow cycle with 1ms delay per step
