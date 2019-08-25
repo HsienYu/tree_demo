6c1ea4b9c6faa0348b4e01acdc39fab271948ec5
H.Y.C <chenghsienyu@aim.com>
2019-08-25 21:26:46 +0800

    quick testing

--- demo.py
+++ demo.py
@@ -30,14 +30,15 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=Fal
 
 while True:
 
-    for i in range(255):
-        pixels.fill((i, i, i))
+i == 0
+if i == 0:
+    i += 1
+    pixels.fill(i, i, i)
+    pixels.show()
+    elif i == 255:
+        i -= 1
+        pixels.fill(i, i, i)
         pixels.show()
-        if i == 255:
-            for i in reversed(range(255)):
-                pixels.fill((i, i, i))
-                pixels.show()
-        print(i)
 time.sleep(1)
 
 #    rainbow_cycle(0.001)    # rainbow cycle with 1ms delay per step
