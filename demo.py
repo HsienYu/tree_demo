048c6ae3a05ac3ccfa4d48b5b04a955185c6271a
H.Y.C <chenghsienyu@aim.com>
2019-08-25 21:21:10 +0800

    print i

--- demo.py
+++ demo.py
@@ -37,7 +37,7 @@ while True:
             for i in reversed(range(255)):
                 pixels.fill((i, i, i))
                 pixels.show()
-
+        print(i)
 time.sleep(1)
 
 #    rainbow_cycle(0.001)    # rainbow cycle with 1ms delay per step
