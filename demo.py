d947c96bffeacc75ef9287de7c219334d7e10158
H.Y.C <chenghsienyu@aim.com>
2019-08-26 02:00:15 +0800

    update

--- demo.py
+++ demo.py
@@ -25,19 +25,19 @@ def wheel(pos):
     if pos < 0 or pos > 255:
         r = g = b = 0
     elif pos < 85:
-        r = int(pos * 1)
-        g = int(255 - pos*1)
+        r = int(pos * 6)
+        g = int(255 - pos*6)
         b = 0
     elif pos < 170:
         pos -= 85
-        r = int(255 - pos*1)
+        r = int(255 - pos*6)
         g = 0
-        b = int(pos*1)
+        b = int(pos*6)
     else:
         pos -= 170
         r = 0
-        g = int(pos*1)
-        b = int(255 - pos*1)
+        g = int(pos*6)
+        b = int(255 - pos*6)
     return (r, 255, 255) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
 
 
